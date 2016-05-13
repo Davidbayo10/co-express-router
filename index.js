5673d0ae631dfbc0ddae00ae5a218f5649a8cc13
David Bayo <dbayo@finametrix.com>
2016-05-14 01:30:22 +0200

    module and tests

---
+++ index.js
@@ -0,0 +1,110 @@
+'use strict';
+const co = require('co');
+const methods = require('methods');
+
+function isAppObj(obj) {
+	return typeof obj.lazyrouter === 'function';
+}
+
+function isGenerator(f) {
+	return typeof f === 'function' && Object.getPrototypeOf(f) !== Object.getPrototypeOf(Function);
+}
+
+function flatten(arr, ret) {
+	ret = ret || [];
+	for (let elem of arr) {
+		if (Array.isArray(elem)) {
+      flatten(elem, ret);
+    } else {
+      ret.push(elem);
+    }
+	}
+
+  return ret;
+};
+
+function wrapCallback(callback, type) {
+	// only wrap callbacks if they are generators
+	if (isGenerator(callback)) {
+		const coCallback = co.wrap(callback);
+		debugger
+		if (type === 'param') {
+			return function (req, res, next, value) {
+				coCallback(req, res, next, value, function (err) {
+					if (err) next(err);
+				});
+			};
+		}
+
+		if (type === 'use') {
+			// check callback arity to find out if it's error-handling middleware
+			if (callback.length > 3) {
+				return function (err, req, res, next) {
+					coCallback(err, req, res, next).catch(err);
+				};
+			} else {
+				return function (req, res, next) {
+					coCallback(req, res, next).catch(next);
+				};
+			}
+		}
+	} else {
+		return callback;
+	}
+}
+
+function patchRoute(router) {
+	const originalRoute = router.route;
+	return function route(path) {
+		const route = originalRoute.call(router, path);
+		methods.concat('all').forEach(function (method) {
+			const f = route[method];
+			route[method] = function () {
+				const callbacks = flatten([].slice.call(arguments, 0)).map(function (callback) {
+					return wrapCallback(callback, 'use');
+				});
+
+				return f.apply(route, callbacks);
+			}
+		});
+
+		return route;
+	};
+}
+
+function patchUse(router) {
+	const originalUse = router.use;
+	return function (path, fn) {
+		if (typeof path === 'function') {
+			return originalUse.call(router, wrapCallback(path, 'use'));
+		} else {
+			return originalUse.call(router, path, wrapCallback(fn, 'use'));
+		}
+	};
+}
+
+function patchParam(router) {
+	const originalParam = router.param;
+	return function (name, fn) {
+		// only get involved when the first parameter is a string and the second is a callable
+		if (typeof name === 'string' && typeof fn === 'function') {
+			return originalParam.call(router, name, wrapCallback(fn, 'param'));
+		} else {
+			return originalParam.apply(router, [].slice.call(arguments, 0));
+		}
+	};
+}
+
+function patch(router) {
+	if (isAppObj(router)) {
+		const app = router;
+		app.lazyrouter();
+		router = app._router;
+	}
+	router.route = patchRoute(router);
+	router.use = patchUse(router);
+	router.param = patchParam(router);
+	return router;
+}
+
+module.exports = patch;
---
+++ test/index-test.js
@@ -0,0 +1,108 @@
+'use strict';
+const express = require('express');
+const supertest = require('supertest-as-promised');
+require('jasmine-co').install();
+
+function asyncText(err, text, cb) {
+  setImmediate(function() {
+    cb(err, text);
+  });
+}
+
+function thunk(err, text) {
+  return function(cb) {
+    asyncText(err, text, cb);
+  };
+}
+
+describe('co-express-router', function () {
+  let app;
+  let request;
+
+  beforeEach(function () {
+    app = express();
+    require('../index.js')(app);
+    request = supertest.agent(app);
+  });
+
+  it('supports a single generator route', function* () {
+    const text = 'works'
+    app.get('/', function* (req, res) {
+      res.send(text);
+    });
+
+    const res = yield request.get('/').toPromise();
+    expect(res).toBeTruthy();
+    expect(res.status).toBe(200);
+    expect(res.text).toBe(text);
+  });
+
+  it('supports multiple generator routes', function* () {
+    app.get('/', function* (req, res, next) {
+      req.val = yield thunk(null, 'thunk');
+      next();
+    }, function* (req, res, next) {
+      req.val += yield thunk(null, 'thunk');
+      next();
+    }, function* (req, res) {
+      res.send(req.val + 'func');
+    });
+
+    const res = yield request.get('/').toPromise();
+    expect(res).toBeTruthy();
+    expect(res.status).toBe(200);
+  });
+
+  it('doesn\'t alter application object', function* () {
+    app.get('/', function* (req, res, next) {
+      res.send('it works!');
+    });
+
+    app.set('it', 'works!');
+
+    const res = yield request.get('/').toPromise();
+    expect(res).toBeTruthy();
+    expect(res.status).toBe(200);
+    expect(res.text).toBe('it works!');
+    expect(app.get('it')).toBe('works!');
+  });
+
+  it('supports error routes', function* () {
+    app.get('/', function* (req, res, next) {
+      const val = yield thunk(new Error('thunk error'));
+      res.send(val);
+    });
+
+    app.use(function (err, req, res, next) {
+      if (err && err.message === 'thunk error') {
+        res.send('caught');
+      } else {
+        next(err);
+      }
+    });
+
+    const res = yield request.get('/').toPromise();
+    expect(res).toBeTruthy();
+    expect(res.text).toBe('caught');
+  });
+
+  it('supports app.route()', function* () {
+    const books = app.route('/books');
+
+    books.get(function* (req, res, next) {
+      req.val = yield thunk(null, 'thunk');
+      next();
+    }, function* (req, res, next) {
+      req.val += yield thunk(null, 'thunk');
+      next();
+    }, function* (req, res) {
+      res.send(req.val + 'func');
+    });
+
+
+    const res = yield request.get('/books').toPromise();
+    expect(res).toBeTruthy();
+    expect(res.status).toBe(200);
+    expect(res.text).toBe('thunkthunkfunc');
+  });
+});
