c44027828e621e36c1672e700b281be5d7c15779
David Bayo <dbayo@finametrix.com>
2017-01-15 15:22:22 +0100

    more descriptive var name for param patcher

--- src/patchers/param.js
+++ src/patchers/param.js
@@ -4,14 +4,14 @@ const util = require('../util');
 const wrapCallback = require('../common').wrapCallback;
 
 function patchParam(router) {
-  const originalParam = router.param;
+  const expressParam = router.param;
   // only get involved when the first parameter is a string and the second is a callable
   return function (name, fn) {
     if (util.isString(name) && util.isFunction(fn)) {
-      return originalParam.call(router, name, wrapCallback(fn, 'param'));
+      return expressParam.call(router, name, wrapCallback(fn, 'param'));
     }
 
-    return originalParam.apply(router, [].slice.call(arguments, 0));
+    return expressParam.call(router, arguments[0]);
   };
 }
 
